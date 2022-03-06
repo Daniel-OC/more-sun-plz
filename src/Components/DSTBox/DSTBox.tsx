9ca57211799eccdf197bb9a35dfb91ea3b16e32e
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-03-06 11:16:46 -0800

    Create dst time box and have it render when appropriate

--- src/Components/DSTBox/DSTBox.tsx
+++ src/Components/DSTBox/DSTBox.tsx
@@ -17,16 +17,16 @@ interface Props {
 const DSTBox = (props: Props) => {
   const notDST = 
     <section>
-      <h2>It is currently Standard Time. If we made it Daylight Savings Time year round it would currently be {props.dstDay.currentTime}.</h2>
-      <p>The sun would have risen at {props.dstDay.sunrise} and set at {props.dstDay.sunset}</p>
-      <p>You would have gotten {props.dstDay.totalSun} hours of sunlight today.</p>
+      <h2>It is currently Standard Time. If we made it Daylight Savings Time year round it would currently be {props.dstDay.date.toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}.</h2>
+      <p>The sun would have risen at {new Date(props.dstDay.sunrise).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})} and set at {new Date(props.dstDay.sunset).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}</p>
+      <p>You would have gotten {props.dstDay.totalSun.toFixed(2)} hours of sunlight today.</p>
     </section> 
 
   const yesDST = 
     <section>
-      <h2>It is currently Daylight Savings Time. It is currently {props.standardDay.currentTime}.</h2>
-      <p>The sun rose at {props.standardDay.sunrise} and will set at {props.standardDay.sunset}</p>
-      <p>You got {props.standardDay.totalSun} hours of sunlight today.</p>
+      <h2>It is currently {props.dstDay.date.toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}, Daylight Savings Time.</h2>
+      <p>The sun rose at {new Date(props.dstDay.sunrise).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})} and will set at {new Date(props.dstDay.sunset).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}</p>
+      <p>You got {props.dstDay.totalSun.toFixed(2)} hours of sunlight today.</p>
     </section>
   
   return (
