98e5af2281f41f0859a3f81709c1ef145edf9ea0
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-03-06 16:46:08 -0800

    Add error handling for api calls

--- src/apiCall.tsx
+++ src/apiCall.tsx
@@ -1,6 +1,18 @@
+const checkForError = (response: Response) => {
+  if (response.ok) {
+    return response.json()
+  } else if ((!response.ok && response.status >= 400) && response.status < 500) {
+    throw (`${response.status} Error. Sorry, the page you're looking for doesn't exist.`)
+  } else if (!response.ok && response.status >= 500) {
+    throw (`${response.status} Error. Something went wrong. Please try again!`)
+  } else if (!response.ok){
+    throw (`${response.status} Error. Something went wrong! We're not sure either, sorry!!`)
+  }
+}
+
 const getSunriseAndSunset = (date: string) => {
   return fetch(`https://api.sunrise-sunset.org/json?lat=47.6062&lng=-122.3321&date=${date}&formatted=0`)
-  .then(response => response.json())
+  .then(response => checkForError(response))
   .then(data => data.results)
 }
 
